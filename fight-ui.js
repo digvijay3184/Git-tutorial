4a28f19b24b3f67745cd9190f4fd13d7aaf7f614
Digvijay Singh <digvijaysingh4040@gmail.com>
2025-12-17 06:57:33 +0530

    one more update for better understanding

--- fight-ui.js
+++ fight-ui.js
@@ -2,3 +2,4 @@ const v1 = "this a  new file for fight ui";
 console.log("fight-ui.js file changed");
 console.log("This is a change in fight-ui.js");
 console.log(v1) ;
+console.log("one more change for better understanding");
